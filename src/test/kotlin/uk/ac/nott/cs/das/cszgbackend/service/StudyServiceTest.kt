0742efbecd6f3ae3b9f29b2e59c7f7ab652f06c3
Ian Knight <ian.knight2@nottingham.ac.uk>
2021-04-28 14:36:49 +0100

    Create skeleton for report tests

--- src/test/kotlin/uk/ac/nott/cs/das/cszgbackend/service/StudyServiceTest.kt
+++ src/test/kotlin/uk/ac/nott/cs/das/cszgbackend/service/StudyServiceTest.kt
@@ -33,105 +33,124 @@ class StudyServiceTest {
     }
 
     @Nested
-    @DisplayName("When repo has data")
-    inner class StudyHasData {
-
-        @Test
-        @DisplayName("Then getAllStudies returns all the studies")
-        fun getAllStudies() {
-            every { studyRepo.findAll() } returns mutableListOf(Study(title = "Test", reports = mutableSetOf()))
-            val studies = service.getAllStudies()
-            assertTrue { studies is Either.Right }
-
-            studies as Either.Right
-            assertEquals(1, studies.value.count())
-        }
-
+    @DisplayName("Given StudyRepository")
+    inner class StudyRepo {
         @Nested
-        @DisplayName("When existing UUID is given")
-        inner class GoodUuid {
+        @DisplayName("When repo has data")
+        inner class StudyHasData {
             @Test
-            @DisplayName("Then getStudy returns the study")
-            fun getStudy() {
-                val study = Study(title = "Test", reports = mutableSetOf())
-                every { studyRepo.findById(study.id) } returns Optional.of(study)
+            @DisplayName("Then getAllStudies returns all the studies")
+            fun getAllStudies() {
+                every { studyRepo.findAll() } returns mutableListOf(Study(title = "Test", reports = mutableSetOf()))
+                val studies = service.getAllStudies()
+                assertTrue { studies is Either.Right }
 
-                val retrievedStudy = service.getStudy(study.id)
-                assertTrue { retrievedStudy is Either.Right }
-
-                retrievedStudy as Either.Right
-                assertEquals("Test", retrievedStudy.value.title)
-                assertEquals(0, retrievedStudy.value.reports.size)
+                studies as Either.Right
+                assertEquals(1, studies.value.count())
             }
+
+            @Nested
+            @DisplayName("When existing UUID is given")
+            inner class GoodUuid {
+                @Test
+                @DisplayName("Then getStudy returns the study")
+                fun getStudy() {
+                    val study = Study(title = "Test", reports = mutableSetOf())
+                    every { studyRepo.findById(study.id) } returns Optional.of(study)
+
+                    val retrievedStudy = service.getStudy(study.id)
+                    assertTrue { retrievedStudy is Either.Right }
+
+                    retrievedStudy as Either.Right
+                    assertEquals("Test", retrievedStudy.value.title)
+                    assertEquals(0, retrievedStudy.value.reports.size)
+                }
+            }
+
+            @Nested
+            @DisplayName("When non-existent UUID is given")
+            inner class BadUuid {
+                @Test
+                @DisplayName("Then getStudy returns 404 error")
+                fun getStudy() {
+                    every { studyRepo.findById(any()) } returns Optional.empty()
+                    val retrievedStudy = service.getStudy(UUID.randomUUID())
+                    assertTrue { retrievedStudy is Either.Left }
+
+                    retrievedStudy as Either.Left
+                    assertEquals(HttpStatus.NOT_FOUND, retrievedStudy.value.status)
+                }
+            }
+
         }
 
         @Nested
-        @DisplayName("When non-existent UUID is given")
-        inner class BadUuid {
+        @DisplayName("When repo has no data")
+        inner class StudyNoData {
             @Test
-            @DisplayName("Then getStudy returns 404 error")
+            @DisplayName("Then getAllStudies returns an empty iterable")
+            fun getAllStudies() {
+                every { studyRepo.findAll() } returns mutableListOf()
+                val studies = service.getAllStudies()
+                assertTrue { studies is Either.Right }
+
+                studies as Either.Right
+                assertEquals(0, studies.value.count())
+            }
+
+            @Test
+            @DisplayName("Then getStudy returns a 404 error")
             fun getStudy() {
                 every { studyRepo.findById(any()) } returns Optional.empty()
-                val retrievedStudy = service.getStudy(UUID.randomUUID())
-                assertTrue { retrievedStudy is Either.Left }
+                val study = service.getStudy(UUID.randomUUID())
+                assertTrue { study is Either.Left }
 
-                retrievedStudy as Either.Left
-                assertEquals(HttpStatus.NOT_FOUND, retrievedStudy.value.status)
+                study as Either.Left
+                assertEquals(HttpStatus.NOT_FOUND, study.value.status)
             }
         }
 
-    }
+        @Nested
+        @DisplayName("When repo fails to read data")
+        inner class RepoBroken {
+            @Test
+            @DisplayName("Then getAllStudies returns a 500 error")
+            fun getAllStudies() {
+                every { studyRepo.findAll() } throws IOException()
+                val studies = service.getAllStudies()
+                assertTrue { studies is Either.Left }
 
-    @Nested
-    @DisplayName("When repo has no data")
-    inner class StudyNoData {
+                studies as Either.Left
+                assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, studies.value.status)
+            }
 
-        @Test
-        @DisplayName("Then getAllStudies returns an empty iterable")
-        fun getAllStudies() {
-            every { studyRepo.findAll() } returns mutableListOf()
-            val studies = service.getAllStudies()
-            assertTrue { studies is Either.Right }
+            @Test
+            @DisplayName("Then getStudy returns a 500 error")
+            fun getStudy() {
+                every { studyRepo.findById(any()) } throws IOException()
+                val study = service.getStudy(UUID.randomUUID())
+                assertTrue(study is Either.Left)
 
-            studies as Either.Right
-            assertEquals(0, studies.value.count())
-        }
-
-        @Test
-        @DisplayName("Then getStudy returns a 404 error")
-        fun getStudy() {
-            every { studyRepo.findById(any()) } returns Optional.empty()
-            val study = service.getStudy(UUID.randomUUID())
-            assertTrue { study is Either.Left }
-
-            study as Either.Left
-            assertEquals(HttpStatus.NOT_FOUND, study.value.status)
+                study as Either.Left
+                assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, study.value.status)
+            }
         }
     }
 
     @Nested
-    @DisplayName("When repo fails to read data")
-    inner class RepoBroken {
-        @Test
-        @DisplayName("Then getAllStudies returns a 500 error")
-        fun getAllStudies() {
-            every { studyRepo.findAll() } throws IOException()
-            val studies = service.getAllStudies()
-            assertTrue { studies is Either.Left }
+    @DisplayName("Given ReportRepository")
+    inner class ReportRepo {
 
-            studies as Either.Left
-            assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, studies.value.status)
-        }
+        @Nested
+        @DisplayName("When repo has data")
+        inner class HasData
 
-        @Test
-        @DisplayName("Then getStudy returns a 500 error")
-        fun getStudy() {
-            every { studyRepo.findById(any()) } throws IOException()
-            val study = service.getStudy(UUID.randomUUID())
-            assertTrue(study is Either.Left)
+        @Nested
+        @DisplayName("When repo has no data")
+        inner class NoData
 
-            study as Either.Left
-            assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, study.value.status)
-        }
+        @Nested
+        @DisplayName("When repo is broken")
+        inner class RepoBroken
     }
 }
