0cbe16186a62c09d692d7ff5a3041a4358ae6552
Ian Knight <ian.knight.1990@gmail.com>
2021-04-29 09:13:56 +0100

    Add post methods to controller

--- src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/StudiesController.kt
+++ src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/StudiesController.kt
@@ -1,9 +1,7 @@
 package uk.ac.nott.cs.das.cszgbackend.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import uk.ac.nott.cs.das.cszgbackend.model.study.Study
 import uk.ac.nott.cs.das.cszgbackend.service.StudyService
 import uk.ac.nott.cs.das.cszgx.returnOrThrow
 import java.util.*
@@ -16,4 +14,11 @@ class StudiesController(private val service: StudyService) {
 
     @GetMapping("/{id}")
     suspend fun getStudy(@PathVariable id: UUID) = service.getStudy(id).returnOrThrow()
+
+    @PostMapping
+    suspend fun addStudy(@RequestBody study: Study) = service.addStudy(study).returnOrThrow()
+
+    @PostMapping("/{id}/link")
+    suspend fun linkReport(@PathVariable studyId: UUID, @RequestParam(name = "report") reportId: UUID) =
+        service.associateStudyReport(studyId, reportId).returnOrThrow()
 }
