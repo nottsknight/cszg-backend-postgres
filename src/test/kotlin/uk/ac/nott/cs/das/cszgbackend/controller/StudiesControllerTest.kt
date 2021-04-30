003336b3b98636bd60247830aa64be1a78dbdab3
Ian Knight <ian.knight.1990@gmail.com>
2021-04-30 11:06:42 +0100

    Starts adding tests for StudiesController

--- src/test/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/StudiesControllerTest.kt
+++ src/test/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/StudiesControllerTest.kt
@@ -1,12 +1,25 @@
 package uk.ac.nott.cs.das.cszgbackend.controller
 
+import arrow.core.Either
 import com.ninjasquad.springmockk.MockkBean
+import io.mockk.coEvery
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.web.server.ResponseStatusException
 import uk.ac.nott.cs.das.cszgbackend.service.StudyService
+import kotlin.test.assertNotNull
+
 
 @ExtendWith(SpringExtension::class)
 @WebMvcTest
@@ -17,4 +30,41 @@ class StudiesControllerTest {
 
     @Autowired
     private lateinit var controller: StudiesController
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Nested
+    @DisplayName("When the context is instantiated")
+    inner class ContextLoad {
+        @Test
+        @DisplayName("Then the context should be created")
+        fun testContextLoads() {
+            assertNotNull(controller)
+        }
+    }
+
+    @Nested
+    @DisplayName("When GET /studies")
+    inner class GetStudies {
+        @Test
+        @DisplayName("Then the controller returns the list of studies if the service works")
+        fun getAllStudies(): Unit = runBlocking {
+            coEvery { service.getAllStudies() } returns Either.Right(listOf())
+            mockMvc.perform(get("/studies")).andExpect {
+                status().isOk
+                content().contentType("application/json")
+                content().json("[]")
+            }
+        }
+
+        @Test
+        @DisplayName("Then the controller returns a 500 error if the service breaks")
+        fun getAllStudiesBad(): Unit = runBlocking {
+            coEvery { service.getAllStudies() } returns Either.Left(ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR))
+            mockMvc.perform(get("/studies")).andExpect {
+                status().isInternalServerError
+            }
+        }
+    }
 }
