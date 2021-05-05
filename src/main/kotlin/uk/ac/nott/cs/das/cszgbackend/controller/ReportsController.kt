15af2518220b74389318c37434a8bc99aa5d028c
Ian Knight <ian.knight.1990@gmail.com>
2021-05-05 21:46:27 +0100

    Enable study-report linking on the reports controller

--- src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/ReportsController.kt
+++ src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/ReportsController.kt
@@ -3,6 +3,7 @@ package uk.ac.nott.cs.das.cszgbackend.controller
 import org.springframework.web.bind.annotation.*
 import uk.ac.nott.cs.das.cszgbackend.model.study.Report
 import uk.ac.nott.cs.das.cszgbackend.model.study.ReportDto
+import uk.ac.nott.cs.das.cszgbackend.model.study.StudyDto
 import uk.ac.nott.cs.das.cszgbackend.service.StudyReportService
 import uk.ac.nott.cs.das.cszgx.returnOrThrow
 import java.util.*
@@ -16,7 +17,13 @@ class ReportsController(private val service: StudyReportService) {
     @GetMapping("/{id}")
     suspend fun getReport(@PathVariable id: UUID) = service.getReport(id).map { ReportDto.fromDao(it) }.returnOrThrow()
 
-    @PostMapping("/reports")
+    @PostMapping
     suspend fun addReport(@RequestBody report: ReportDto) =
         Report.fromDto(report).let { service.addReport(it) }.map { ReportDto.fromDao(it) }.returnOrThrow()
+
+    @PostMapping("/{reportId}/link/{studyId}")
+    suspend fun linkReportStudy(@PathVariable reportId: UUID, @PathVariable studyId: UUID) =
+        service.associateStudyReport(studyId, reportId)
+            .map { (s, r) -> StudyDto.fromDao(s) to ReportDto.fromDao(r) }
+            .returnOrThrow()
 }
