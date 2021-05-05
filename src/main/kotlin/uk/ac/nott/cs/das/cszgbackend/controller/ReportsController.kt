b802199f4ff7c8e435a4461511fa7d55b50648db
Ian Knight <ian.knight.1990@gmail.com>
2021-05-05 21:43:56 +0100

    Convert to send and receive report DTOs

--- src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/ReportsController.kt
+++ src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/controller/ReportsController.kt
@@ -2,6 +2,7 @@ package uk.ac.nott.cs.das.cszgbackend.controller
 
 import org.springframework.web.bind.annotation.*
 import uk.ac.nott.cs.das.cszgbackend.model.study.Report
+import uk.ac.nott.cs.das.cszgbackend.model.study.ReportDto
 import uk.ac.nott.cs.das.cszgbackend.service.StudyReportService
 import uk.ac.nott.cs.das.cszgx.returnOrThrow
 import java.util.*
@@ -10,11 +11,12 @@ import java.util.*
 @RequestMapping("/reports")
 class ReportsController(private val service: StudyReportService) {
     @GetMapping
-    suspend fun getAllReports() = service.getAllReports().returnOrThrow()
+    suspend fun getAllReports() = service.getAllReports().map { it.map { r -> ReportDto.fromDao(r) } }.returnOrThrow()
 
     @GetMapping("/{id}")
-    suspend fun getReport(@PathVariable id: UUID) = service.getReport(id).returnOrThrow()
+    suspend fun getReport(@PathVariable id: UUID) = service.getReport(id).map { ReportDto.fromDao(it) }.returnOrThrow()
 
     @PostMapping("/reports")
-    suspend fun addReport(@RequestBody report: Report) = service.addReport(report).returnOrThrow()
+    suspend fun addReport(@RequestBody report: ReportDto) =
+        Report.fromDto(report).let { service.addReport(it) }.map { ReportDto.fromDao(it) }.returnOrThrow()
 }
