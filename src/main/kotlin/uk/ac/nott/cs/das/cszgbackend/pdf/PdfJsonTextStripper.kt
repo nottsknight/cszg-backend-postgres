bfbc2890d39b346997936cff242aee6ec4dcfd27
Ian Knight <ian.knight.1990@gmail.com>
2021-05-06 08:57:14 +0100

    Fill in writing of individual text objects

--- src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/pdf/PdfJsonTextStripper.kt
+++ src/main/kotlin/uk/ac/nott/cs/das/cszgbackend/pdf/PdfJsonTextStripper.kt
@@ -19,7 +19,13 @@ class PdfJsonTextStripper : PDFTextStripper() {
     override fun endPage(page: PDPage?) = writeString("""]},""")
     override fun endDocument(document: PDDocument?) = writeString("]}")
 
-    override fun writeString(text: String?, textPositions: MutableList<TextPosition>?) {
-        super.writeString(text, textPositions)
+    override fun writeString(text: String, textPositions: MutableList<TextPosition>) {
+        val fontName = textPositions[0].font.name
+        val fontSize = textPositions[0].fontSizeInPt
+        val x1 = textPositions.minOf { it.x }
+        val y1 = textPositions.minOf { it.y }
+        val x2 = textPositions.maxOf { it.endX }
+        val y2 = textPositions.maxOf { it.endY }
+        writeString("""{"text":"$text","fontName":"$fontName","fontSize":$fontSize,"x1":$x1,"y1":$y1,"x2":$x2,"y2":$y2}""")
     }
 }
