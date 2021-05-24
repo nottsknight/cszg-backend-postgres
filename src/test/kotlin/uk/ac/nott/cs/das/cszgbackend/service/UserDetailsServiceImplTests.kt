33d7b754c4a4fd806d085516ea134650dff64cfb
Ian Knight <ian.knight.1990@gmail.com>
2021-05-24 13:23:54 +0100

    Convert UserDetailsServiceImplTests.kt

--- src/test/kotlin/uk/ac/nott/cs/das/cszgbackend/service/UserDetailsServiceImplTests.kt
+++ src/test/kotlin/uk/ac/nott/cs/das/cszgbackend/service/UserDetailsServiceImplTests.kt
@@ -1,10 +1,12 @@
 package uk.ac.nott.cs.das.cszgbackend.service
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -14,32 +16,26 @@ import uk.ac.nott.cs.das.cszgbackend.model.participant.Participant
 import uk.ac.nott.cs.das.cszgbackend.model.participant.ParticipantRepository
 import java.util.*
 
-@ExtendWith(MockKExtension::class)
-@DisplayName("UserDetailsServiceImpl")
-class UserDetailsServiceImplTests {
-    @MockK
-    private lateinit var repo: ParticipantRepository
+class UserDetailsServiceImplTests : DescribeSpec({
+    describe("UserDetailsServiceImpl") {
+        lateinit var repo: ParticipantRepository
+        lateinit var service: UserDetailsServiceImpl
 
-    private lateinit var service: UserDetailsServiceImpl
+        beforeEach {
+            repo = mockk()
+            service = UserDetailsServiceImpl(repo)
+        }
 
-    @BeforeEach
-    fun setUp() {
-        service = UserDetailsServiceImpl(repo)
+        it("should return the UserDetails if the username exists") {
+            every { repo.findByUsername(any()) } answers { Optional.of(Participant(username = firstArg())) }
+            val participant = service.loadUserByUsername("abcd")
+            participant.username.shouldBe("abcd")
+        }
+
+        it("should throw UsernameNotFoundException if the username doesn't exist") {
+            every { repo.findByUsername(any()) } returns Optional.empty()
+            val ex = shouldThrow<UsernameNotFoundException> { service.loadUserByUsername("abcd") }
+            ex.message.shouldBe("abcd")
+        }
     }
-
-    @Test
-    @DisplayName("Should return UserDetails if the username exists")
-    fun usernameExists() {
-        every { repo.findByUsername(any()) } answers { Optional.of(Participant(username = firstArg())) }
-        val participant = service.loadUserByUsername("abcd")
-        participant.username.shouldBe("abcd")
-    }
-
-    @Test
-    @DisplayName("Should throw UsernameNotFoundException if the username doesn't exist")
-    fun usernameDoesNotExist() {
-        every { repo.findByUsername(any()) } returns Optional.empty()
-        val ex = shouldThrow<UsernameNotFoundException> { service.loadUserByUsername("abcd") }
-        ex.message.shouldBe("abcd")
-    }
-}
+})
